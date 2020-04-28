0c910c4de44ec16f3f1fa4f31ffa4fcca25ce358
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-04-28 20:50:54 +0530

    peak ,is empty ,is full for stack

--- StackUsingArray/StackUsingArray/main.cpp
+++ StackUsingArray/StackUsingArray/main.cpp
@@ -14,7 +14,20 @@ struct stack
     int top;
     int *s;
 };
-
+void create(stack *st)
+{
+  cout<<"Enter Size of stack:";
+    cin>>st->size;
+    st->s=new int[st->size];
+    st->top=-1;
+}
+void display(struct stack st)
+{
+    for(int i=st.top;i>=0;i--)
+    {
+        cout<<st.s[i]<<endl;
+    }
+}
 void push(stack *st,int x)
 {
     if(st->top==st->size-1)
@@ -37,14 +50,42 @@ int pop(stack *st)
     }
     return x;
 }
+int peak(struct stack st,int pos)
+{
+    int x=-1;
+    if(st.top-pos+1)
+        cout<<"invalid position";
+    else{
+        x=st.s[st.top-pos+1];
+    }
+    return x;
+}
+int stackTop(struct stack st)
+{
+    if(st.top==-1)return -1;
+    else return st.s[st.top];
+}
+int isEmpty(struct stack st)
+{
+    if(st.top==-1)return 1;
+    else return 0;
+}
+int isFull(struct stack st)
+{
+    if(st.top==st.size-1)return 1;
+    else return 0;
+}
 
 int main()
 {
     struct stack st;
-    cout<<"Enter Size of stack:";
-    cin>>st.size;
-    st.s=new int[st.size];
-    st.top=-1;
+    create(&st);
+    push(&st, 10);
+    push(&st, 18);
+    push(&st, 20);
+    pop(&st);
+    display(st);
+    
     
     return 0;
 }
