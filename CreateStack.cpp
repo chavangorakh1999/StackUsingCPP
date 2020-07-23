8cd5f763e31566146c318c397e7b155f793cf148
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-07-23 15:33:35 +0530

    testing problems for Developers Hunt

--- CreateStack.cpp
+++ CreateStack.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 // struct Node
 // {
@@ -10,8 +11,35 @@ void display()
 {
 
 }
-int main()
-{
-    cout<<"hello";
+// int main()
+// {
+//     cout<<"Hello World \\n";
+//     return 0;
+// }
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    string Name,greet;
+    int time;
+    cout<<"Enter Name:";
+    cin>>Name;
+    cout<<"Enter Time:";
+    cin>>time;
+    
+        if(time>0 && time<12)
+        {
+            greet="Good Morning";
+            
+        }
+        else if(time>12 && time<16)
+        {
+            greet="Good Afternoon";
+        }
+        else if(time>16)
+        {
+            greet="Good Evening";
+        }
+    
+    cout<<greet<<" "<<Name<<" "<<"sir.";
+    
     return 0;
 }
