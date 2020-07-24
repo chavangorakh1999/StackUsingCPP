3682a9743ced9664d4dda50cdb6571adf04f9d23
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-07-24 19:18:35 +0530

    Toc revision

--- CreateStack.cpp
+++ CreateStack.cpp
@@ -11,35 +11,35 @@ void display()
 {
 
 }
-// int main()
-// {
-//     cout<<"Hello World \\n";
-//     return 0;
-// }
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    string Name,greet;
-    int time;
-    cout<<"Enter Name:";
-    cin>>Name;
-    cout<<"Enter Time:";
-    cin>>time;
-    
-        if(time>0 && time<12)
-        {
-            greet="Good Morning";
-            
-        }
-        else if(time>12 && time<16)
-        {
-            greet="Good Afternoon";
-        }
-        else if(time>16)
-        {
-            greet="Good Evening";
-        }
-    
-    cout<<greet<<" "<<Name<<" "<<"sir.";
-    
+int main()
+{
+    cout<<"Hello World \\n";
     return 0;
 }
+// int main() {
+//     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+//     string Name,greet;
+//     int time;
+//     cout<<"Enter Name:";
+//     cin>>Name;
+//     cout<<"Enter Time:";
+//     cin>>time;
+    
+//         if(time>0 && time<12)
+//         {
+//             greet="Good Morning";
+            
+//         }
+//         else if(time>12 && time<16)
+//         {
+//             greet="Good Afternoon";
+//         }
+//         else if(time>16)
+//         {
+//             greet="Good Evening";
+//         }
+    
+//     cout<<greet<<" "<<Name<<" "<<"sir.";
+    
+//     return 0;
+// }
