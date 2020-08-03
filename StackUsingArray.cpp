e9b860cf697acf8e6431cb9f25ee122d33d2e906
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-08-03 17:02:21 +0530

    staeted stack today

--- StackUsingArray.cpp
+++ StackUsingArray.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
 using namespace std;
 
-class stack()
+class stack
 {
+    public:
+    int size;
+    int top;
+    int *st;
+}s;
 
-}
 
 int main()
 {
+    cout<<"Enter size:";
+    cin>>s.size;
+    s.st=new int [s.size];
+    s.top=-1;
     return 0;
 }
 
